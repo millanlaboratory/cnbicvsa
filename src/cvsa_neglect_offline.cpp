148205ad0417e96d9cff5ab5c314ecf1520b6697
Luca Tonin <luca.tonin@gmail.com>
2017-08-09 18:27:29 +0200

    Added text with trial number at the beginning of each trial

--- src/cvsa_neglect_offline.cpp
+++ src/cvsa_neglect_offline.cpp
@@ -7,6 +7,8 @@
 #include <cnbidraw/EventKeyboard.hpp>
 #include <cnbidraw/Cross.hpp>
 #include <cnbidraw/Arrow.hpp>
+#include <cnbidraw/Font.hpp>
+#include <cnbidraw/String.hpp>
 #include "InfiniteBar.hpp"
 
 
@@ -20,9 +22,12 @@
 #define CNBICVSA_BAR_HEIGHT	0.1f
 #define CNBICVSA_BAR_NOBJS	7
 #define CNBICVSA_BAR_WOBJS	0.05f
+#define CNBICVSA_TEXT_SIZE	0.1f
+#define CNBICVSA_TEXT_COLOR	dtk_white
 
-#define CNBICVSA_FIX_TIME	    1000.0f
+#define CNBICVSA_FIX_TIME	    2000.0f
 #define CNBICVSA_CUE_TIME	    500.0f
+#define CNBICVSA_ITI_TIME	    1000.0f
 #define CNBICVSA_CFDBK_TIME_MIN	    3000.0f
 #define CNBICVSA_CFDBK_TIME_MAX	    5000.0f
 #define CNBICVSA_BAR_VELOCITY	    0.01f
@@ -37,6 +42,8 @@ int main(int argc, char** argv) {
     draw::Events* 	events;
     draw::Cross*	fix;
     draw::Arrow*	cue;
+	draw::Font*		font;
+    draw::String*	txttrl;
     cvsa::InfiniteBar*	bar;
 
     // Cnbiloop definitions
@@ -44,13 +51,16 @@ int main(int argc, char** argv) {
     // Protocol definitions
     unsigned int idtrial = 1;
     unsigned int ntrials = 30;
+	std::string  clabel;
 
 
     // Draw elements initialization
     fix    = new draw::Cross(CNBICVSA_FIX_SIZE, CNBICVSA_FIX_THICK, CNBICVSA_FIX_COLOR);
     cue    = new draw::Arrow(CNBICVSA_CUE_WIDTH, CNBICVSA_CUE_HEIGHT, CNBICVSA_CUE_COLOR);
     bar	   = new cvsa::InfiniteBar(CNBICVSA_BAR_WIDTH, CNBICVSA_BAR_HEIGHT, 
-				   CNBICVSA_BAR_NOBJS, CNBICVSA_BAR_WOBJS);
+								   CNBICVSA_BAR_NOBJS, CNBICVSA_BAR_WOBJS);
+	font   = new draw::Font("arial:style=bold");
+	txttrl = new draw::String(CNBICVSA_TEXT_SIZE, CNBICVSA_TEXT_COLOR);
     engine = new draw::Engine;
     events = new draw::Events(engine);
 
@@ -62,9 +72,12 @@ int main(int argc, char** argv) {
     bar->Create();
     bar->Move(0.0f, -0.2f);
     cue->Rotate(40.0f);
+	txttrl->SetFont(font);
+	txttrl->Create();
     engine->Add("fix", fix);
     engine->Add("cue", cue);
     engine->Add("bar", bar);
+    engine->Add("txttrl", txttrl);
 
     std::random_device rd;  //Will be used to obtain a seed for the random number engine
     std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
@@ -72,45 +85,51 @@ int main(int argc, char** argv) {
 					     CNBICVSA_CFDBK_TIME_MAX);
    
     while( (idtrial <= ntrials) & engine->IsRunning() ) {
-	printf("[cnbicvsa] - Trial %d/%d\n", idtrial++, ntrials);
+		
+		// Inter-Trial interval
+		clabel = "Trial " + std::to_string(idtrial++) + "/" + std::to_string(ntrials);
+		printf("[cnbicvsa] - %s\n", clabel.c_str());
+		txttrl->SetText(clabel);
+		fix->Hide();
+		cue->Hide();
+		txttrl->Show();
+		CcTime::Sleep(CNBICVSA_ITI_TIME);
 
-	// Fixation scene
-	fix->Show();
-	cue->Hide();
-	CcTime::Sleep(CNBICVSA_FIX_TIME);
-
-	// Cue scene
-	fix->Hide();
-	cue->Show();
-	CcTime::Sleep(CNBICVSA_CUE_TIME);
-
-	// Continuous feedback scene
-	fix->Show();
-	cue->Hide();
-
-	CcTimeValue tic;
-    	CcTime::Tic(&tic);
-	auto ctime = rndtime(gen);
-	while(true) {
-	    bar->Update(-CNBICVSA_BAR_VELOCITY);
-
-	    if(CcTime::Toc(&tic) > ctime) {
-		printf("Elapsed: %f\n", CcTime::Toc(&tic));
-		break;
-	    }
-
-	    CcTime::Sleep(50.0f);
-	}
-
+		// Fixation scene
+		txttrl->Hide();
+		fix->Show();
+		cue->Hide();
+		CcTime::Sleep(CNBICVSA_FIX_TIME);
 	
-	// Discrete feedback
-	fix->Show();
-	cue->Hide();
+		// Cue scene
+		fix->Hide();
+		cue->Show();
+		CcTime::Sleep(CNBICVSA_CUE_TIME);
 	
-	// ITI
-	fix->Show();
-	cue->Hide();
-
+		// Continuous feedback scene
+		fix->Show();
+		cue->Hide();
+	
+		CcTimeValue tic;
+		CcTime::Tic(&tic);
+		auto ctime = rndtime(gen);
+		while(true) {
+		    bar->Update(-CNBICVSA_BAR_VELOCITY);
+	
+		    if(CcTime::Toc(&tic) > ctime) {
+				printf("Elapsed: %f\n", CcTime::Toc(&tic));
+				break;
+		    }
+	
+		    CcTime::Sleep(50.0f);
+		}
+	
+		
+		// Discrete feedback
+		fix->Show();
+		cue->Hide();
+		
+	
     }
     
     engine->Stop();
